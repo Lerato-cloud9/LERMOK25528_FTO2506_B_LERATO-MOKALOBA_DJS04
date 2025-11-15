d72384ff4dead37c734d355e5ef67c314d976b30
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-11-15 16:17:07 +0200

    Added JSdocs comments for Genre file

---
+++ src/components/GenreFilter.jsx
@@ -0,0 +1,9 @@
+/**
+ * GenreFilter component - Provides genre filtering with multi-select chips
+ * 
+ * @param {Object} props
+ * @param {Array<number>} props.selectedGenres - Array of selected genre IDs
+ * @param {Function} props.onChange - Callback function when genre selection changes
+ * @param {Array<Object>} props.genres - Array of all available genres
+ * @returns {JSX.Element} The rendered genre filter interface
+ */
