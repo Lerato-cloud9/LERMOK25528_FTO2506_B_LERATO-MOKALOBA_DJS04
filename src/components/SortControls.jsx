186284522f82648a77d94348ffaa49b5904b7439
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-11-15 15:57:05 +0200

    added JSdocs comments

---
+++ src/components/SortControls.jsx
@@ -0,0 +1,8 @@
+/**
+ * SortControls component - Provides a dropdown to sort podcasts by different criteria
+ * 
+ * @param {Object} props
+ * @param {string} props.value - Current sort option ('newest', 'oldest', 'a-z', 'z-a')
+ * @param {Function} props.onChange - Callback function when sort option changes
+ * @returns {JSX.Element} The rendered sort dropdown
+ */
