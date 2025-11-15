74e5a8ca6e84df6ae3a1e132cd8f8eba8b5cad4a
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-11-15 16:42:27 +0200

    add a genre toggle function for selecting and unselecting genres

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -7,3 +7,14 @@
  * @param {Function} props.onPageChange - Callback function when page changes
  * @returns {JSX.Element} The rendered pagination controls
  */
+
+export default function GenreFilter({ selectedGenres, onChange, genres }) {
+  // This function turns a genre on or off when the user clicks it
+  // genreId = the ID of the genre we are selecting or unselecting
+  const handleGenreToggle = (genreId) => {
+    if (selectedGenres.includes(genreId)) {                // If the genre is already selected, remove it from the list
+      onChange(selectedGenres.filter(id => id !== genreId));
+    } else {                                                // If it’s not selected yet, add it to the list
+      onChange([...selectedGenres, genreId]);
+    }
+  };
