320ba8913f5169cffb4a7b3613d451c87e5a8bcb
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-11-15 16:24:03 +0200

    added genre toggle logic to GenreFilter component

--- src/components/GenreFilter.jsx
+++ src/components/GenreFilter.jsx
@@ -7,3 +7,16 @@
  * @param {Array<Object>} props.genres - Array of all available genres
  * @returns {JSX.Element} The rendered genre filter interface
  */
+
+export default function GenreFilter({ selectedGenres, onChange, genres }) {
+
+  // Toggle a genre on or off when the user clicks on it
+  // genreId = the ID of the genre being selected or unselected
+
+  const handleGenreToggle = (genreId) => {                   // If the genre is already selected, remove it from the list
+    if (selectedGenres.includes(genreId)) {
+      onChange(selectedGenres.filter(id => id !== genreId));
+    } else {                                                  // Otherwise, add the new genre to the selected list
+      onChange([...selectedGenres, genreId]);
+    }
+  };
