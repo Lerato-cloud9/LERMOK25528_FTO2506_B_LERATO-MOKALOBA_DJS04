04d879b6088947c0053e89f1ce4de33193082240
Lerato Mokaloba <mokalobalerato@gmail.com>
2025-11-15 16:39:03 +0200

    added JSdocs comments

---
+++ src/components/Pagination.jsx
@@ -0,0 +1,9 @@
+/**
+ * Pagination component - Provides page navigation controls
+ * 
+ * @param {Object} props
+ * @param {number} props.currentPage - The current active page number
+ * @param {number} props.totalPages - Total number of pages available
+ * @param {Function} props.onPageChange - Callback function when page changes
+ * @returns {JSX.Element} The rendered pagination controls
+ */
